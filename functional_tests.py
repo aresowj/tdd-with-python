f5ff54e7311781b650582f085fb4e9bf7bdaf274
aresowj <aresowj@gmail.com>
2016-03-07 21:26:16 +0000

    Initial commit for the example in Test-Driven Development with Python

---
+++ functional_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+

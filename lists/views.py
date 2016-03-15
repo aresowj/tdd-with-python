84fa4dd07ea97bc2e4f5f4c8e6d792486ce3b58a
Ares Ou <aresowj@gmail.com>
2016-03-14 23:36:28 -0700

    added List

--- lists/views.py
+++ lists/views.py
@@ -3,16 +3,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
     return render(request, 'home.html')
 
+
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')

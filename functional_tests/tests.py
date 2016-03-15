ef1c7f91996a1db8a53b042e1e11283edd4ba984
Ares Ou <aresowj@gmail.com>
2016-03-14 21:56:31 -0700

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -32,7 +31,10 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
         inputbox.send_keys('Buy peacock feathers')
+
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -41,5 +43,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+        # a new user Francis comes along to the site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,18 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 from lists.models import Item
 
 
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect(reverse('view_list'))
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
 
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
